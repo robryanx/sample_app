3381217b8dc1040797f7813b2f91d88c0f4a7102
Rob Ryan <robbiz233@hotmail.com>
2011-05-05 12:03:04 +1000

    next commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	# handle title
+	def title
+		base_title = "Ruby Sample App"
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} | #{@title}"
+		end
+	end
+
 end
